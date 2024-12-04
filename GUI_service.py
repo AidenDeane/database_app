e91a647c42307866c4bb388773ab236c4beb5a81
SirPaniniee <67246205+AidenDeane@users.noreply.github.com>
2024-12-04 11:44:03 -0500

    Added more stuff

--- GUI_service.py
+++ GUI_service.py
@@ -1,6 +1,7 @@
 import PySimpleGUI as sg
-
+import time
 passwordChallenge = False
+
 
 ## Password challenge
 '''----------'''
@@ -8,21 +9,29 @@ while passwordChallenge == False:
     passwordLayout = [
         [sg.Text('Please enter your password!')],
         [sg.Input(key='-pw-'), sg.Button('OK')]
-    ]
+        ]
     passwordPage = sg.Window('Password',passwordLayout)
     values, event = passwordPage.read()
-    print(values, event)
+    #print(values, event)
     if event == {'-pw-': 'pass'}: # if key -pw- has value of 'pass'
         passwordChallenge = True # closes the window
 '''----------'''
 
 ## Homepage
 '''---------'''
+
 homepageLayout = [ 
-    [sg.Text("Hello!",font=('Arial',20)  )  ]
+    [sg.Push(), sg.Text("Welcome to the Universal Business Portal!",font=('Arial',20)), sg.Push()],
+    [sg.Button('Inventory',enable_events=True)]
      ]
 
-homepage = sg.Window('Home', homepageLayout, size=(720,720))
+inventoryLayout = [
+    [sg.Text('Testing')]
+]
 
-if passwordChallenge == True:
-    homepage.read() #Launches the window
+homepage = sg.Window('Home', homepageLayout, size=(720,720))
+inventoryPage = sg.Window('Inv',inventoryLayout,)
+
+while passwordChallenge == True:
+    event,values = homepage.read() #Launches the window
+    
